9a29849a9ff6b609db5eef16af37570926c40682
kemochka <kemaevak@yandex.ru>
2023-10-17 17:05:38 +0300

    1. Модульные тесты. [#159435 #452975]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class ConverterTest {
-
     @Test
     void whenConvert140RblThen2Euro() {
         float in = 140;
