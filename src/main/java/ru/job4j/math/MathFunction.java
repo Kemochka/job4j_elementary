f50c36a0fa1f0e0480dd5895f179ad946d8f7156
kemochka <kemaevak@yandex.ru>
2023-10-19 23:19:33 +0300

    1.1. Что такое import [#504773 #453077]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -9,32 +9,11 @@ public class MathFunction {
         return first * second;
     }
 
-    public static double sumAndMultiply(double first, double second) {
-        return ru.job4j.math.MathFunction.sum(first, second)
-                + ru.job4j.math.MathFunction.multiply(first, second);
-    }
-
     public static double sub(double first, double second) {
         return first - second;
     }
 
     public static double divide(double first, double second) {
         return first / second;
-    }
-
-    public static double subAndDivide(double first, double second) {
-        return ru.job4j.math.MathFunction.sub(first, second)
-        + ru.job4j.math.MathFunction.divide(first, second);
-    }
-
-    public static double allOperations(double first, double second) {
-        return sum(first, second) + multiply(first, second)
-                + sub(first, second) + divide(first, second);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
-        System.out.println("Результат расчета равен: " + subAndDivide(10, 20));
-        System.out.println("Результат расчета равен: " + allOperations(10, 20));
     }
 }
