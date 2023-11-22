b29ade9ba5cc90286d8ced28e1ff31da8608f9aa
kemochka <kemaevak@yandex.ru>
2023-11-22 20:44:28 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,33 +8,27 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        assertThat(expected).isEqualTo(dist, withPrecision(0.01));
     }
 
     @Test
     void when30to20then1() {
         double expected = 1;
-        int x1 = 3;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(3, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        assertThat(expected).isEqualTo(dist, withPrecision(0.01));
     }
 
     @Test
     void when60to23then5() {
         double expected = 5;
-        int x1 = 6;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(6, 0);
+        Point b = new Point(2, 3);
+        double dist = a.distance(b);
+        assertThat(expected).isEqualTo(dist, withPrecision(0.01));
     }
 }
