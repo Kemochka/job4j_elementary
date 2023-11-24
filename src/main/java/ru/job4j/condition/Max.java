cc22b01f5a5bf62eb1d719f0636f1b078c8fff35
kemochka <kemaevak@yandex.ru>
2023-11-24 20:41:55 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,7 +1,16 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left > right ? left : right;
+
+    public int max(int left, int right) {
+        return Math.max(left, right);
+    }
+
+    public int max(int left, int right, int c) {
+        return max(left, max(right, c));
+    }
+
+    public int max(int left, int right, int c, int d) {
+        return max(left, right, max(c, d));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -4,39 +4,34 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MaxTest {
-    @Test
-    void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
-        int expected = 2;
-        assertThat(result).isEqualTo(expected);
-    }
 
     @Test
     void whenMax4To3Then4() {
         int left = 4;
         int right = 3;
-        int result = Max.max(left, right);
+        int result = new Max().max(left, right);
         int expected = 4;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenMax7To9Then9() {
-        int left = 7;
-        int right = 9;
-        int result = Max.max(left, right);
-        int expected = 9;
+    void whenMax1To2To3Then3() {
+        int left = 1;
+        int right = 2;
+        int c = 3;
+        int result = new Max().max(left, right, c);
+        int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenMax1To1Then1() {
-        int left = 1;
-        int right = 1;
-        int result = Max.max(left, right);
-        int expected = 1;
+    void whenMax2To6To7To9Then9() {
+        int left = 7;
+        int right = 9;
+        int c = 6;
+        int d = 2;
+        int result = new Max().max(left, right, c, d);
+        int expected = 9;
         assertThat(result).isEqualTo(expected);
     }
 }
